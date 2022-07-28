ba80ee1aded52bbe85c32cc628f6cc68fe3bfbda
Areeb Ghani <91328326+AreebGhani@users.noreply.github.com>
2022-07-29 00:20:38 +0500

    react-node-express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "/react-app/build")))
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
